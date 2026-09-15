bf43a6b776f0d59a9dd598f9461fd5d2c08ee2cb
Durkkadharshini <durkkadharshini@gamil.com>
2026-09-15 22:13:38 +0530

    Prepare Django settings for deployment

--- hospital_management/settings.py
+++ hospital_management/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-dt(ke_4+iw&v$fhh(_(^uqry3*8hwo#nzl2%ndf$u96v4_hi_*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
